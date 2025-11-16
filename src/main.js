a3b3a36d49cca42bc41c8bdf3a0bb6b6bffb3e41
fakhreddinelargou <largofakhrddin@gmail.com>
2025-11-16 22:21:48 +0100

    complated form

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 let workers = []
 let posts = []
+
+
+//    <label class="flex flex-col w-full mb-4" for="job">
+//        <p class="text-gray-600 mb-2">Job Title*</p> 
+//         <input class="bg-gray-100 h-10 pl-2 rounded-md mb-1 border border-gray-400 outline-none" type="text" id="job" placeholder="Job Title">
+//         <p id="checkJb" class="text-red-500 text-[.6rem] ">somethink wrong</p>
+//     </label>
+//       <label class="flex flex-col w-full mb-4" for="job">
+//        <p class="text-gray-600 mb-2">Company*</p> 
+//         <input class="bg-gray-100 h-10 pl-2 rounded-md mb-1 border border-gray-400 outline-none" type="text" id="company" placeholder="Co..">
+//         <p id="checkCp" class="text-red-500 text-[.6rem] ">somethink wrong</p>
+//     </label>
+//       <label class="flex flex-col w-full mb-4" for="date">
+//        <p class="text-gray-600 mb-2">Date*</p> 
+//         <input class="bg-gray-100 h-10 pl-2 rounded-md mb-1 border border-gray-400 outline-none" type="date" id="date">
+//         <p id="checkDt" class="text-red-500 text-[.6rem] ">somethink wrong</p>
+//     </label>
+//       <label class="flex flex-col w-full mb-4" for="job">
+//        <p class="text-gray-600 mb-2">Description*</p> 
+//          <textarea placeholder="Write..." class="bg-gray-100 h-20 pl-2 rounded-md mb-1 border border-gray-400  outline-none"></textarea>
+//         <p id="checkCp" class="text-red-500 text-[.6rem] ">somethink wrong</p>
+//     </label>
